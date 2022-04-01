018f63ac220a83fbfcd42d5be1364e6d60aa54dd
ashishsah1000 <ashishsah1000@gamil.com>
2022-04-01 07:43:23 +0530

    added morgon bcrypt and session packages

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+
 const router = express.Router();
 
 const {
@@ -7,7 +9,7 @@ const {
   findUser,
 } = require("../controllers/userController");
 
-router.route("/user/new").post(newUser);
+router.route("/user/new", cors()).post(newUser);
 router.route("/user/check").get(findUser);
 router.route("/user").get(userCheck); //for checking the route
 
--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
-
+// const cors = require("cors");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const bcryptjs = require("bcryptjs");
+const session = require("express-session");
+const morgon = require("morgan");
 // setting up the env file
 dotenv.config({ path: "./config/config.env" });
 
 // connecting to database
 connectDatabase();
+app.use(morgon("dev"));
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
+app.use(cookieParser("secretcode"));
 app.listen(process.env.PORT, () => {
   console.log(
     "server started at port",
@@ -15,4 +30,13 @@ app.listen(process.env.PORT, () => {
     "in mode : ",
     process.env.NODE_ENV
   );
+});
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
